4889fcab646b554a48b6522fe56c132f085cc0cc
cchura94 <cchura.cpc@gmail.com>
2021-06-02 20:56:24 -0400

    Broker Configuracion puerto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var aedes = require('aedes')()
 var server = require('net').createServer(aedes.handle)
-var port = 1883
+var port = process.env.PORT || 1883
 
 server.listen(port, function () {
 	console.log('server listening on port', port)
