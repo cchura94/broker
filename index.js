314083af16e4d44af5d7b32e10fcd97c4920515b
cchura94 <cchura.cpc@gmail.com>
2021-06-10 10:20:49 -0400

    MQTT

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 var aedes = require('aedes')()
 var server = require('net').createServer(aedes.handle)
 var port = process.env.PORT || 1883
@@ -6,9 +5,9 @@ var port = process.env.PORT || 1883
 server.listen(port, function () {
 	console.log('server listening on port', port)
 })
-*/
 
 
+/*
 const aedes = require('aedes')()
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
@@ -19,3 +18,4 @@ ws.createServer({ server: httpServer }, aedes.handle)
 httpServer.listen(port, function () {
   console.log('websocket server listening on port ', port)
 })
+*/
